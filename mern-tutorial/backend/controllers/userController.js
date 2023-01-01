f76ddaca02001681f6fa3aa895a8f1b494cbadcf
Tony <tonyngmengkiat@gmail.com>
2023-01-01 13:29:06 +0800

    add user register and login methods endpoints

---
+++ mern-tutorial/backend/controllers/userController.js
@@ -0,0 +1,82 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const asyncHandler = require('express-async-handler');
+const User = require('../models/userModel');
+
+//@desc    Get user data
+//@route   GET /api/users/me
+//@access  Public
+const getMe = asyncHandler(async (req, res) => {
+    res.json({ message: "Display user data" });
+});
+
+//@desc    Authenticate a user
+//@route   POST /api/users/login
+//@access  Public
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    // Check for user email
+    const user = await User.findOne({ email });
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+        })
+    } else {
+        res.status(400);
+        throw new Error("Invalid credentials");
+    };
+});
+
+
+//@desc    Register new user
+//@route   POST /api/users
+//@access  Public
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+
+    // Check if all fields are present
+    if (!name || !email || !password) {
+        res.status(400);
+        throw new Error("Please add all fields");
+    };
+
+    //  Check if user exist
+    const userExists = await User.findOne({ email })
+
+    if (userExists) {
+        res.status(400);
+        throw new Error("User already exists");
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    if (user) {
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+        })
+    } else {
+        res.status(400);
+        throw new Error("Error, user was not created");
+    }
+
+});
+
+module.exports = {
+    getMe,
+    loginUser,
+    registerUser
+};
--- mern-tutorial/backend/server.js
+++ mern-tutorial/backend/server.js
@@ -13,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/goals', require('./routes/goalRoutes.js'));
+app.use('/api/users', require('./routes/userRoutes.js'));
 
 app.use(errorHandler);
 
